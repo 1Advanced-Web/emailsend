7ba33d53954cbc8829fedcfd4eddaa88d610da4d
1Advanced-Web <59934871+1Advanced-Web@users.noreply.github.com>
2020-12-15 11:11:36 +0300

    Update send.php

--- send.php
+++ send.php
@@ -21,7 +21,7 @@ if(!isset($_POST['fio']) and !isset($_POST['email'])){
  $email = urldecode($email);
  $fio = trim($fio);
  $email = trim($email);
- if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
+ if (mail("mtrubchanin@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: maksimtrubcanin@gmail.com \r\n")){
  echo "Сообщение успешно отправлено";
  } else {
  echo "При отправке сообщения возникли ошибки";
